8538b55573e43be078ec3ca656c33ec89821636c
zee-german <franz.lutzenberger@gmail.com>
2016-05-11 09:12:06 +0200

    album cards

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
-  $('.ui.card .image').dimmer({on: 'hover'});
+  $('.ui.card .image').dimmer({on: 'hover'})
+  $('.ui.rating').rating({maxRating: 5})
+  $('.ui.button').popup()
+
+  $('.button.view').on('click', (function() {
+    $('#album').fadeOut('slow', function() {
+      $('#album_items').fadeIn('slow')
+    })
+  }))
+
+  $('.button.back').on('click', (function() {
+    $('#album_items').fadeOut('slow', function() {
+      $('#album').fadeIn('slow')
+    })
+  }))
+
 })
